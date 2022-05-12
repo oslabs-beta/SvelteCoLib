852fdcd858129b01b27b993f6616a044ac9ba0ba
tbarry16 <tim.barry12@gmail.com>
2022-05-12 13:30:36 -0400

    Accordion basic functionality working

---
+++ src/lib/components/Accordion/AccordionPanel.svelte
@@ -0,0 +1,34 @@
+<script>
+// @ts-nocheck
+    export let panelContent = ''
+    export let isOpen
+    export let panelID
+    export let labeledBy;
+</script>
+
+
+<div 
+    class='accordion-panel' 
+    role="region" 
+    class:open-panel={isOpen} 
+    id="{panelID}"
+    aria-labelledby={labeledBy}
+>
+{#if isOpen}
+    <p>{panelContent}</p>
+{/if}
+</div>
+
+<style>
+
+    .accordion-panel {
+        height: 0;
+        width: 100%;
+    }
+
+    .open-panel {
+        height: auto;
+        border: solid 1px black;
+    }
+
+</style>
