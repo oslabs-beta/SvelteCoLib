3523c54972b3f030d812e9eed3e641e755fa1e6b
tbarry16 <tim.barry12@gmail.com>
2022-05-12 09:56:39 -0400

    Basic structure of accordion component written

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,15 @@
+<script>
+    
+    import AccordionItem from "../lib/components/Accordion/AccordionItem.svelte";
+    
+    import Accordion from '../lib/components/Accordion/Accordion.svelte'
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+
+<Accordion>
+    <AccordionItem/>
+    <AccordionItem/>
+</Accordion>
