bc862279ae8fccb9bb535bca84acd5d852204beb
swierkopa <swierkopa@gmail.com>
2022-05-12 17:55:39 -0400

    input box done

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,10 @@
+<script>
+  import Input from '../lib/components/Input.svelte';
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<Input label="Name" id="name" type="text" placeholder="eg. John Smith"/><br><br>
+<Input label="Email" id="email" type="email" placeholder="eg. jsmith@gmail.com"/><br><br>
+<Input label="Phone" id="phone" type="tel" placeholder="eg. 123-456-7890"/><br><br>
