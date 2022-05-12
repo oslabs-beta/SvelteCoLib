6f5ec04fcfdd1b9fa33dfe8d6f24ecaa2e09f2d2
Nurbek <21njouraboev@gmail.com>
2022-05-12 17:55:29 -0400

    checkbox accessible

---
+++ src/lib/components/Form.svelte
@@ -0,0 +1,13 @@
+<script>
+	// @ts-nocheck
+	let checked = false;
+</script>
+
+<!-- <label aria-label="form">
+	<input type="form" />
+</label> -->
+<style>
+	:global(.main__checkbox) {
+		margin: 0.5rem;
+	}
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,9 @@
+<script>
+	import Checkbox from '../lib/components/Checkbox.svelte';
+	import Form from '../lib/components/Form.svelte';
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<Checkbox />
+<Form />
